4f3ee89c1d08aa797b9d9d5c23a44755b0bd98a4
Richard Wong <richard.wong.com@gmail.com>
2017-05-22 15:57:36 -0400

    Use rspec in generator to create test factories and model specs. Add Course model.

---
+++ spec/factories/courses.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :course do
+    course_code "SE 101"
+    title "Introduction to Software Engineering"
+    description "Taught by P.L. in the Fall of 2016."
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name "Learner Larry"
+    email "larry@downtolearn.com"
+    password_digest "deadbeefdeadbeef"
+  end
+end
