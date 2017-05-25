27a25137042eed5ab07cde63c6d8acd3dd3562b8
Josh Chorlton <jchorlton@gmail.com>
2017-05-24 22:49:28 -0400

    Add Nav bar

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Nav from '../Nav';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-          Josh
+          <Nav />
       </div>
     );
   }
