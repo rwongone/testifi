dbe84bb661b75b44ec2029b7085b1635c970b858
Josh Chorlton <jchorlton@gmail.com>
2017-05-24 23:30:10 -0400

    Add admin signup form

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import AdminSignup from '../AdminSignup';
 import Nav from '../Nav';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Nav />
-      </div>
-    );
-  }
+    render() {
+        // TODO only display admin signup when necessary
+        return (
+                <div>
+                    <Nav />
+                    <AdminSignup />
+                </div>
+                );
+    }
 }
 
 export default App;
