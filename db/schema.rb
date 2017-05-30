0b8b6da4092b919f81c36cd8d27a18c6eeaf0018
Richard Wong <richard.wong.com@gmail.com>
2017-05-30 13:39:08 -0400

    Add filepath column to Submission
    Use filepath attribute to specify where the source of files to copy into
    the test execution container.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524200327) do
+ActiveRecord::Schema.define(version: 20170530173726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170524200327) do
     t.string "language", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "filepath"
     t.index ["problem_id"], name: "index_submissions_on_problem_id"
     t.index ["user_id"], name: "index_submissions_on_user_id"
   end
