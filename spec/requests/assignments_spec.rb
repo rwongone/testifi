339905eb213471ba810e7b0b204d14a3585ed2d3
Richard Wong <richard.wong.com@gmail.com>
2017-05-30 20:45:26 -0400

    Add controllers, fix specs. (#13)
    * Add factories for specs
    
    * Add CoursesController, fix SubmissionsController
    
    * No 404 during auth, plus refactor
    
    * Fix show specs for requests

--- spec/requests/assignments_spec.rb
+++ spec/requests/assignments_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Assignments", type: :request do
+  let(:user) { create(:user) }
+  let(:course) { create(:course) }
+  let!(:assignment) { create(:assignment, course_id: course.id) }
+  let(:headers) { {} }
+  let(:auth_header) { {'HTTP_AUTHORIZATION': 'Bearer X'} }
+
+  before do
+    allow(Auth).to receive(:decode).and_return({ 'user_id' => user.id })
+  end
+
   describe "GET /assignments" do
-    it "works! (now write some real specs)" do
-      get assignments_index_path
+    it "returns the Assignment as JSON" do
+      get "/assignments/#{assignment.id}", headers: headers.merge(auth_header)
       expect(response).to have_http_status(200)
+      expect(response.body).to eq(assignment.to_json)
     end
   end
 end
--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Courses", type: :request do
+  let(:user) { create(:user) }
+  let!(:course) { create(:course) }
+  let(:headers) { {} }
+  let(:auth_header) { {'HTTP_AUTHORIZATION': 'Bearer X'} }
+
+  before do
+    allow(Auth).to receive(:decode).and_return({ 'user_id' => user.id })
+  end
+
   describe "GET /courses" do
-    it "works! (now write some real specs)" do
-      get courses_index_path
+    it "returns the Course as JSON" do
+      get "/courses/#{course.id}", headers: headers.merge(auth_header)
       expect(response).to have_http_status(200)
+      expect(response.body).to eq(course.to_json)
     end
   end
 end
