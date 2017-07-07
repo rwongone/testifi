68eed313ae4dc61d544d880b88374d74f2494d2c
Josh Chorlton <jchorlton@gmail.com>
2017-07-07 00:41:15 -0400

    more spec

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -28,17 +28,43 @@ RSpec.describe "User authentication", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    # it "returns a 400 for an existing user with incorrect password" do
-    #   post("/api/login", params: {email: email, password: "the_wrong_password"})
-    #   expect(ActiveSupport::JSON.decode(response.body)).to eq({})
-    #   expect(response).to have_http_status(400)
-    # end
+    it "returns user info for an existing user with a google_id" do
+      validator = instance_double("GoogleIDToken::Validator")
+      expect(GoogleIDToken::Validator).to receive(:new).and_return(validator).exactly(2)
+      expect(validator).to receive(:check).with(code, google_client.id, google_client.id).and_return({
+          "sub"=>google_id,
+          "name"=>name,
+          "email"=>email
+      }).exactly(2)
 
-    # it "returns a 400 for a non-existent user" do
-    #   post("/api/login", params: {email: "whatisthis", password: "doesn't matter"})
-    #   expect(ActiveSupport::JSON.decode(response.body)).to eq({})
-    #   expect(response).to have_http_status(400)
-    # end
+      get("/api/users/oauth/google", params: {code: code})
+      parsed = ActiveSupport::JSON.decode(response.body)
+      expect(parsed['name']).to eq(name)
+      expect(parsed['email']).to eq(email)
+      expect(parsed['google_id']).to eq(google_id)
+      expect(cookies['Authorization']).not_to be_empty
+      expect(response).to have_http_status(200)
+
+      first_id = parsed['id']
+
+      get("/api/users/oauth/google", params: {code: code})
+      parsed = ActiveSupport::JSON.decode(response.body)
+      expect(parsed['name']).to eq(name)
+      expect(parsed['email']).to eq(email)
+      expect(parsed['google_id']).to eq(google_id)
+      expect(parsed['id']).to eq(first_id)
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns a 403 for an invalid token" do
+      validator = instance_double("GoogleIDToken::Validator")
+      expect(GoogleIDToken::Validator).to receive(:new).and_return(validator)
+      expect(validator).to receive(:check).with(code, google_client.id, google_client.id).and_return(false)
+      expect(validator).to receive(:problem).and_return("invalid token")
+
+      get("/api/users/oauth/google", params: {code: code})
+      expect(response).to have_http_status(403)
+    end
   end
 
   def google_client
