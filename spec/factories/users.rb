e6a39fe392f9c2de83a52ca8525e9e3286497e7e
Richard Wong <richard.wong.com@gmail.com>
2017-07-07 00:52:12 -0400

    Fixing factories.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,19 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:student] do
     name { 'Larry Learner' }
     email { 'larry@downtolearn.com' }
-    google_id { '3432432kdfsfds324' }
+    google_id { 'google_id123' }
     admin { false }
+  end
+
+  trait :teacher do
+    name { 'P B' }
+    email { 'pb@teacher.com' }
+    google_id { 't_google_id123' }
+    admin { true }
+  end
+
+  trait :github do
+    github_id { 'github_id123' }
   end
 end
