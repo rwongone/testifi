914dd000d15c224558b95c63c79d83a17f2d2417
Matthew Du <du.matthew@gmail.com>
2017-07-08 17:56:54 -0400

    Add course method to get all users

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,14 @@
 class Course < ApplicationRecord
-    has_and_belongs_to_many :students, class_name: "User", join_table: "courses_students", association_foreign_key: "student_id"
-    belongs_to :teacher, class_name: "User", foreign_key: "teacher_id"
+  has_and_belongs_to_many :students, class_name: "User", join_table: "courses_students", association_foreign_key: "student_id"
+  belongs_to :teacher, class_name: "User", foreign_key: "teacher_id"
+
+  has_many :assignments
+
+  def user_ids
+    Set.new(student_ids.push(teacher_id))
+  end
+
+  def users
+    User.where(id: user_ids)
+  end
 end
