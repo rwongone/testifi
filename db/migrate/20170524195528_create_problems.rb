702285cbe78553739d1b03b34ce40b85cbc0ddf9
Matthew Du <du.matthew@gmail.com>
2017-07-08 23:57:40 -0400

    Add solution association

--- db/migrate/20170524195528_create_problems.rb
+++ db/migrate/20170524195528_create_problems.rb
@@ -3,11 +3,13 @@ class CreateProblems < ActiveRecord::Migration[5.1]
     create_table :problems do |t|
       t.string :name
       t.text :description
-      t.string :soln_filename
-      t.binary :soln_file_contents
       t.references :assignment
+      t.bigint :solution_id # this is a foreign key for a canonical submission
 
       t.timestamps
+
+      t.index :solution_id
     end
+
   end
 end
