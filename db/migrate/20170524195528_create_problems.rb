86272bc242d7885817e3bc83df368aca8d6c19eb
Richard Wong <richard.wong.com@gmail.com>
2017-07-08 23:35:00 -0400

    Problems contain the corresponding solution file

--- db/migrate/20170524195528_create_problems.rb
+++ db/migrate/20170524195528_create_problems.rb
@@ -3,6 +3,8 @@ class CreateProblems < ActiveRecord::Migration[5.1]
     create_table :problems do |t|
       t.string :name
       t.text :description
+      t.string :soln_filename
+      t.binary :soln_file_contents
       t.references :assignment
 
       t.timestamps
