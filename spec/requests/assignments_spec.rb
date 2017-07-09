adcd092f6e7d184a20113115e5550aaaef6a94f7
Richard Wong <richard.wong.com@gmail.com>
2017-07-09 15:39:29 -0400

    Resolve merge conflict.

--- spec/requests/assignments_spec.rb
+++ spec/requests/assignments_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe "Assignments", type: :request do
   let(:teacher) { create(:teacher) }
   let(:course) { create(:course) }
   let!(:assignment) { create(:assignment, course_id: course.id) }
+  let(:assignment_create_params) do
+    {
+      "name" => "Assignment 1",
+      "description" => "This course introduces students to databases"
+    }
+  end
 
   context "when a teacher is authenticated" do
     before(:each) do
@@ -33,6 +39,19 @@ RSpec.describe "Assignments", type: :request do
           expect(response.body).to eq(assignment.to_json)
         end
       end
+
+      describe "POST /api/courses/:id/assignments" do
+        it "creates an assignment" do
+          post "/api/courses/#{course.id}/assignments", params: assignment_create_params
+          expect(response).to have_http_status(201)
+
+          expect(json_response).to include(
+            "name" => assignment_create_params["name"],
+            "description" => assignment_create_params["description"],
+            "course_id" => course.id
+          )
+        end
+      end
     end # teacher teaches course
 
     context "and the teacher does not teach the course" do
@@ -41,6 +60,11 @@ RSpec.describe "Assignments", type: :request do
         [
           "/api/assignments/#{assignment.id}",
         ]
+      end
+      let(:restricted_post_endpoints) do
+        {
+          "/api/courses/#{course.id}/assignments" => assignment_create_params,
+        }
       end
 
       describe "GET /api/courses/:course_id/assignments" do
@@ -53,20 +77,33 @@ RSpec.describe "Assignments", type: :request do
 
       # TODO(rwongone): Would we want teachers not to be able to look
       # at assignments that don't belong to their courses?
-      describe "restricted Assignment endpoints" do
+      describe "restricted Submission endpoints" do
         it "are inaccessible" do
           restricted_get_endpoints.each do |endpoint|
             get endpoint
             expect(response).to be_forbidden
           end
+
+          restricted_post_endpoints.each do |endpoint, params|
+            post endpoint, params: params
+            expect(response).to be_forbidden
+          end
         end
       end
+
     end # teacher does not teach course
   end
 
   context "when a student is authenticated" do
     before(:each) do
       authenticate(student)
+    end
+
+    describe "POST /api/courses/:id/assignments" do
+      it "is inaccessible whether or not the student is enrolled" do
+        post "/api/courses/#{course.id}/assignments", params: assignment_create_params
+        expect(response).to be_forbidden
+      end
     end
 
     context "and the student is enrolled" do
@@ -117,25 +154,5 @@ RSpec.describe "Assignments", type: :request do
         end
       end
     end # not enrolled
-  end
-
-  describe "POST /api/courses/:id/assignments" do
-    it "allows teachers to post assignments" do
-      authenticate(teacher)
-
-      params = {
-        "name" => "Assignment 1",
-        "description" => "This course introduces students to databases"
-      }
-
-      post "/api/courses/#{course.id}/assignments", params: params, headers: headers
-      expect(response).to have_http_status(201)
-      parsed = ActiveSupport::JSON.decode(response.body)
-      expect(parsed).to include(
-        "name" => params["name"],
-        "description" => params["description"],
-        "course_id" => course.id
-      )
-    end
   end
 end
