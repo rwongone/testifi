64d7ab26a78912c004fbec29fe69ea24d8b43fa0
Josh Chorlton <jchorl@users.noreply.github.com>
2017-07-11 10:52:37 -0400

    UI for teacher test creation (#39)

---
+++ client/src/components/Test/TestList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import TestTile from './TestTile';
+import TestNew from './TestNew';
+
+class TestList extends Component {
+    static propTypes = {
+        test: ImmutablePropTypes.mapOf(
+                            ImmutablePropTypes.contains({
+                                tests: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+                                    id: PropTypes.number.isRequired
+                                })).isRequired
+                            }).isRequired
+                            ).isRequired,
+        user: ImmutablePropTypes.contains({
+            isAdmin: PropTypes.bool.isRequired,
+        }).isRequired,
+        problemId: PropTypes.number.isRequired
+    }
+
+    render() {
+        const {
+            test,
+            user,
+            problemId
+        } = this.props;
+        const tests = test.getIn([problemId, 'tests']);
+
+        return (
+                <div className="testList">
+                    <h2>Tests</h2>
+                    {
+                    tests.map(t => <TestTile key={ t.get('id') } test={ t } />)
+                    }
+                    {
+                    user.get('isAdmin')
+                    ? <TestNew problemId={ problemId } />
+                    : null
+                    }
+                </div>
+                );
+    }
+}
+
+export default connect(state => ({
+    test: state.test,
+    user: state.user
+}))(TestList);
---
+++ client/src/reducers/test.js
@@ -0,0 +1,36 @@
+import { Map, List, fromJS } from 'immutable';
+import { CREATE_PROBLEM_SUCCESS, RECEIVE_PROBLEMS_SUCCESS } from '../actions/problem';
+import { CREATE_TEST_SUCCESS, RECEIVE_TESTS_SUCCESS } from '../actions/test';
+
+const defaultStateForProblem = Map({
+    fetched: false,
+    tests: List()
+});
+
+export default function(state = Map(), action) {
+    switch (action.type) {
+        case CREATE_TEST_SUCCESS:
+            return state.updateIn([action.problemId, 'tests'], p => p.push(fromJS(action.test)));
+
+        case RECEIVE_PROBLEMS_SUCCESS:
+            for (let a of action.problems) {
+                // there is a race where tests can be fetched before problems
+                if (!state.get(a.id)) {
+                    state = state.set(a.id, defaultStateForProblem);
+                }
+            }
+            return state;
+
+        case CREATE_PROBLEM_SUCCESS:
+            return state.set(action.problem.id, defaultStateForProblem);
+
+        case RECEIVE_TESTS_SUCCESS:
+            return state.set(action.problemId, Map({
+                fetched: true,
+                tests: fromJS(action.tests)
+            }));
+
+        default:
+            return state;
+    }
+}
