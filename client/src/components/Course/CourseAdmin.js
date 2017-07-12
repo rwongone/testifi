5a16262f1399b2d4b6c5699dc6a83f69ee2eb81a
Josh Chorlton <jchorlton@gmail.com>
2017-07-12 16:54:10 -0400

    course admin page

--- client/src/components/Course/CourseAdmin.js
+++ client/src/components/Course/CourseAdmin.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import AssignmentNav from '../Assignment/AssignmentNav';
+import './CourseAdmin.css';
 
 class CourseAdmin extends Component {
     static propTypes = {
@@ -20,10 +21,16 @@ class CourseAdmin extends Component {
         } = this.props;
         const parsedCourseId = parseInt(courseId, 10);
 
+        // TODO add two ways students can be registered in courses (list upload or text field)
+        // TODO add list of pending invitations (with resend button)
+        // TODO add list of registered students in the course
+        // TODO add a way of seeing user progress in the course
         return isAdmin ? (
-                <div>
+                <div className="courseAdmin">
                     <AssignmentNav history={ history } courseId={ parsedCourseId } backEnabled={ true } />
-                    Course admin here
+                    <div className="frame">
+                        <h2>Student Registration</h2>
+                    </div>
                 </div>
                 ) : <Redirect to="/" />;
     }
