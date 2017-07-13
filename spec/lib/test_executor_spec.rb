525078af76b6a3c47ba955c0d8d68d995d38bc62
matthewdu <du.matthew@gmail.com>
2017-07-12 23:21:55 -0400

    Change TestExecutor interface, and change tests expected_output type

--- spec/lib/test_executor_spec.rb
+++ spec/lib/test_executor_spec.rb
@@ -50,23 +50,23 @@ RSpec.describe TestExecutor do
       problem.solution_id = solution.id
     end
 
-    describe ".run_tests" do
+    describe ".correct_submission?" do
       it "executes all Tests for the Submission's Problem using the Submission" do
-        expect(subject).to receive(:run_test).exactly(problem.tests.length).times
+        expect(subject).to receive(:correct_output?).exactly(problem.tests.length).times
 
-        subject.run_tests(solution)
+        subject.correct_submission?(solution)
       end
     end
 
-    describe ".run_test" do
+    describe ".correct_output?" do
       it "returns true if the Submission output matches expected output" do
-        expect(subject.run_test(solution, test_consec_3)).to be true
-        expect(subject.run_test(solution, test_consec_5)).to be true
+        expect(subject.correct_output?(solution, test_consec_3)).to be true
+        expect(subject.correct_output?(solution, test_consec_5)).to be true
       end
 
       it "returns false if the Submission output differs from expected output" do
-        expect(subject.run_test(bad_submission, test_consec_3)).to be false
-        expect(subject.run_test(bad_submission, test_consec_5)).to be false
+        expect(subject.correct_output?(bad_submission, test_consec_3)).to be false
+        expect(subject.correct_output?(bad_submission, test_consec_5)).to be false
       end
 
       it "computes expected output iff expected output is missing on the Test" do
@@ -76,14 +76,14 @@ RSpec.describe TestExecutor do
           once.
           and_call_original
 
-        subject.run_test(bad_submission, test_consec_3)
+        subject.correct_output?(bad_submission, test_consec_3)
         expect(test_consec_3.expected_output).to eq("6\n")
       end
     end
 
-    describe ".output_of" do
+    describe ".run_test" do
       it "returns the result of executing the Test on the Submission code" do
-        output = subject.output_of(solution, test_consec_3)
+        output = subject.run_test(solution, test_consec_3)
         expect(output).to eq("6\n")
       end
     end
