1487ff49e870bb200a8116c70027c4acef8beeda
Josh Chorlton <jchorlton@gmail.com>
2017-07-12 20:46:53 -0400

    invites table WIP

---
+++ db/migrate/20170712220519_create_invites.rb
@@ -0,0 +1,12 @@
+class CreateInvites < ActiveRecord::Migration[5.1]
+  def change
+    create_table :invites, id: :uuid do |t|
+      t.string :email
+      t.bigint :inviter_id
+      t.bigint :course_id
+
+      t.timestamps
+      t.index :inviter_id
+    end
+  end
+end
---
+++ spec/factories/invites.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :invite do
+    email "MyString"
+    invited_by 1
+  end
+end
