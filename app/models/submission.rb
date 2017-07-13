a4cebae77db96fbd1defbbb14f83c732ad661b62
matthewdu <du.matthew@gmail.com>
2017-07-13 14:42:59 -0400

    Fix submission has_many executions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,6 @@
 class Submission < ApplicationRecord
   has_one :db_file, foreign_key: "id", primary_key: "db_file_id"
-  has_one :execution
+  has_many :executions
 
   belongs_to :problem
   belongs_to :user
