ee4eae38d825ba30c79d1390c9693b8f1ab17b91
Josh Chorlton <jchorlton@gmail.com>
2017-07-13 14:35:35 -0400

    input works

--- client/src/components/Course/CourseAdmin.js
+++ client/src/components/Course/CourseAdmin.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import classNames from 'classnames';
+import { Set } from 'immutable';
 import AssignmentNav from '../Assignment/AssignmentNav';
 import './CourseAdmin.css';
 
@@ -13,15 +15,32 @@ class CourseAdmin extends Component {
         isAdmin: PropTypes.bool.isRequired
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            rawEmails: "",
+            parsedEmails: []
+        };
+    }
+
+    emailChange = e => {
+        const parsedEmails = Set(e.target.value.split(/[\s,]+/g)).toList().sort();
+        this.setState({
+            parsedEmails,
+            rawEmails: e.target.value
+        });
+    }
+
     render() {
         const {
             match: { params: { courseId } },
             history,
             isAdmin
         } = this.props;
+        const { parsedEmails, rawEmails } = this.state;
         const parsedCourseId = parseInt(courseId, 10);
+        const showParsedEmails = rawEmails !== '' && parsedEmails.size;
 
-        // TODO add two ways students can be registered in courses (list upload or text field)
         // TODO add list of pending invitations (with resend button)
         // TODO add list of registered students in the course
         // TODO add a way of seeing user progress in the course
@@ -30,6 +49,18 @@ class CourseAdmin extends Component {
                     <AssignmentNav history={ history } courseId={ parsedCourseId } backEnabled={ true } />
                     <div className="frame">
                         <h2>Student Registration</h2>
+                        <div className="filedropSection">
+                            <div className="instructions">
+                                Enter emails into the textbox and then click invite
+                            </div>
+                            <div className="emailsContainer">
+                                <textarea className={ classNames('emailInput', { showParsedEmails }) } onChange={ this.emailChange } value={ rawEmails }/>
+                                <div className={ classNames('emails', 'frame', { showParsedEmails }) }>
+                                    { parsedEmails.map(e => <div key={ e }>{ e }</div>) }
+                                </div>
+                            </div>
+                            <button className="inviteButton">Invite</button>
+                        </div>
                     </div>
                 </div>
                 ) : <Redirect to="/" />;
