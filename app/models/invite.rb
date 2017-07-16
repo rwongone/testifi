c510ee74667ba447ed59472535c51728aa03f1d4
matthewdu <du.matthew@gmail.com>
2017-07-15 22:13:22 -0400

    Add scope for unused Invites

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,6 +3,10 @@ class Invite < ApplicationRecord
   belongs_to :course
   belongs_to :redeemer, class_name: "User", foreign_key: "redeemer_id", optional: true
 
+  def self.unused
+    where(redeemer_id: nil)
+  end
+
   def url
     return Rails.configuration.url + '/redeem/' + self.id
   end
