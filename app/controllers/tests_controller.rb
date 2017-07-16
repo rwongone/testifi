8d7f49ff0847239d423554ef6629dea21165ed7e
matthewdu <du.matthew@gmail.com>
2017-07-16 17:41:38 -0400

    Fix PR comments

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -31,10 +31,10 @@ class TestsController < ApplicationController
     FillExpectedOutputJob.perform_later(test.id)
 
     # Rerun most recent submissions of each user on this new test case
-    newest_submission_by_user = problem.submissions.group(:user_id).maximum(:id).values
-    newest_submission_by_user.delete(problem.solution_id)
-    if newest_submission_by_user.size > 0
-      RunSubmissionsJob.perform_later(*newest_submission_by_user)
+    newest_submission_ids_by_user = problem.submissions.group(:user_id).maximum(:id).values
+    newest_submission_ids_by_user.delete(problem.solution_id)
+    if newest_submission_ids_by_user.size > 0
+      RunSubmissionsJob.perform_later(*newest_submission_ids_by_user)
     end
 
     render status: :created, json: test
