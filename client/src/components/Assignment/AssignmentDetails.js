596b3bea253ff8de13f1958e3f7877554dd55b3c
Josh Chorlton <jchorlton@gmail.com>
2017-07-18 18:16:24 -0400

    allow editing assignment name

--- client/src/components/Assignment/AssignmentDetails.js
+++ client/src/components/Assignment/AssignmentDetails.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import classNames from 'classnames';
 import ProblemList from '../Problem/ProblemList';
+import { updateAssignment } from '../../actions/assignment';
+import './AssignmentDetails.css';
 
 class AssignmentDetails extends Component {
     static propTypes = {
@@ -21,7 +24,18 @@ class AssignmentDetails extends Component {
                 courseId: PropTypes.string.isRequired
             }).isRequired
         }).isRequired,
-        history: PropTypes.object.isRequired
+        dispatch: PropTypes.func.isRequired,
+        history: PropTypes.object.isRequired,
+        isAdmin: PropTypes.bool.isRequired,
+    }
+
+    constructor(props) {
+        super(props);
+        const assignment = this.getAssignment(props);
+        this.state = {
+            nameEdit: false,
+            name: assignment.get('name'),
+        };
     }
 
     getCourseId = () => {
@@ -31,37 +45,83 @@ class AssignmentDetails extends Component {
         return parseInt(courseId, 10);
     }
 
-    getAssignmentId = () => {
+    getAssignment = props => {
         const {
-            match: { params: { assignmentId } }
-        } = this.props;
-        return parseInt(assignmentId, 10);
+            match: { params: { assignmentId } },
+            assignment,
+        } = props;
+        const parsedAssignmentId = parseInt(assignmentId, 10);
+        const courseId = this.getCourseId();
+        return assignment.getIn([courseId, 'assignments']).find(a => a.get('id') === parsedAssignmentId);
+    }
+
+    beginNameEdit = () => {
+        const { isAdmin } = this.props;
+        if (!isAdmin) {
+            return;
+        }
+
+        this.setState({ nameEdit: true });
+    }
+
+    submitNameEdit = () => {
+        const { dispatch } = this.props;
+        const { name } = this.state;
+        const info = this.getAssignment(this.props).toJS();
+        info.name = name;
+        dispatch(updateAssignment(info));
+        this.setState({ nameEdit: false });
+    }
+
+    cancelNameEdit = () => {
+        this.setState({ nameEdit: false });
+    }
+
+    onChange = field => e => {
+        const newState = {};
+        newState[field] = e.target.value;
+        this.setState(newState);
+        e.preventDefault();
     }
 
     render() {
         const {
-            assignment,
-            history
+            history,
+            isAdmin,
         } = this.props;
-        const assignmentId = this.getAssignmentId();
+        const {
+            nameEdit,
+            name,
+        } = this.state;
         const courseId = this.getCourseId();
-        const ass = assignment.getIn([courseId, 'assignments']).find(a => a.get('id') === assignmentId);
+        const ass = this.getAssignment(this.props);
 
         return (
-                <div>
-                    <h1>{ ass.get('name') }</h1>
+                <div className="assignmentDetails">
+                    {
+                    nameEdit
+                    ? (
+                    <div className="nameEdit">
+                        <input type="text" value={ name } onChange={ this.onChange('name') }/>
+                        <button onClick={ this.cancelNameEdit }>Cancel</button>
+                        <button onClick={ this.submitNameEdit }>Edit</button>
+                    </div>
+                    )
+                    : <h1 onClick={ this.beginNameEdit } className={ classNames({ editable: isAdmin }) }>{ ass.get('name') }</h1>
+                    }
                     <div>
                         Description:
                     </div>
                     <div>
                         { ass.get('description') }
                     </div>
-                    <ProblemList courseId={ courseId } assignmentId={ assignmentId } history={ history } />
+                    <ProblemList courseId={ courseId } assignmentId={ ass.get('id') } history={ history } />
                 </div>
                 );
     }
 }
 
 export default connect(state => ({
-    assignment: state.assignment
+    assignment: state.assignment,
+    isAdmin: state.user.get('isAdmin'),
 }))(AssignmentDetails);
