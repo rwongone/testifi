a9011a0778dc00fdf9b835c4d35b265fa03d2785
Josh Chorlton <jchorlton@gmail.com>
2018-01-22 10:13:29 -0500

    Fix github login for real (maybe)

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,5 @@
-export const GITHUB_CLIENT_ID = '82f6832181377b5832e2';
+// different github apps for prod and testing
+export const GITHUB_CLIENT_ID = window.location.href.includes('testifi.me') ? '0a41005f5fdcb30eb24b' : '82f6832181377b5832e2';
 export const NEW_COURSE_ID = -1;
 export const NEW_ASSIGNMENT_ID = -2;
 export const UNKNOWN_USER_ID = -3;
