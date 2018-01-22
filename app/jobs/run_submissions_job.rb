3c741612aea51ee9272389556068238f7a5621a0
Richard Wong <richard.wong.com@gmail.com>
2018-01-22 01:07:58 -0500

    Run solutions before submissions, if executions need to be updated

--- app/jobs/run_submissions_job.rb
+++ app/jobs/run_submissions_job.rb
@@ -4,9 +4,21 @@ class RunSubmissionsJob < ApplicationJob
   queue_as :pending_execution
 
   def perform(*submission_ids)
-    submissions = Submission.includes(:problem).find(submission_ids)
+    submissions = Submission.includes(:problem).find(submission_ids.uniq)
+
+    run_solutions(submissions)
+
     submissions.each do |submission|
       submission.run_tests! if submission.id != submission.problem.solution_id
+    end
+  end
+
+  def run_solutions(submissions)
+    solution_ids = submissions.map { |s| s.problem.solution_id }
+    solutions = Submission.find(solution_ids.uniq)
+
+    solutions.each do |solution|
+      solution.run_tests!
     end
   end
 end
